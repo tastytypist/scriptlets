bc3a6ff4fcde87fa4b8349458ef95ab0c50f831d
tastytypist <70253713+tastytypist@users.noreply.github.com>
2022-12-08 05:52:26 +0000

    Merge scriptlets into one file
    Also expands nitter.js to redirect-hostname.js.

--- nitter.js
+++
@@ -1,4 +0,0 @@
-/// nitter.js
-(function() {
-    window.location.replace("https://nitter.net" + window.location.pathname);
-})();
---
+++ scriptlets.js
@@ -0,0 +1,25 @@
+/// redirect-hostname.js
+/// alias rh.js
+/**
+ * Redirects opened URL by replacing its hostname with the specified hostname.
+ * @param {string} hostname - a valid string representation of a hostname we
+ *                            want to be redirected to
+ * */
+(function() {
+    const hostname = "{{1}}"
+    if (hostname === "" || hostname === "{{1}}") {
+        return;
+    }
+    const valid_hostname = (() => {
+        try {
+            new URL(hostname);
+            return true;
+        } catch (_) {
+            return false;
+        }
+    })();
+    if (!valid_hostname) {
+        return;
+    }
+    window.location.replace(hostname + window.location.pathname);
+})();
