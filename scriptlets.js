b4606263a8a879e3b9675d33d9d5923c51b3798b
tastytypist <70253713+tastytypist@users.noreply.github.com>
2022-12-22 14:11:38 +0000

    Prefer TTV LOL proxy for twitch-video-ad.js

--- scriptlets.js
+++ scriptlets.js
@@ -87,7 +87,7 @@
             //This corrects the background tab buffer bug when switching to the background tab for the first time after an extended period.
             doTwitchPlayerTask(false, false, true, false, false);
         };
-        document.addEventListener('visibilitychange', block, true);
+        document.addEventListener('visibilitychange', process, true);
         document.addEventListener('webkitvisibilitychange', block, true);
         document.addEventListener('mozvisibilitychange', block, true);
         document.addEventListener('hasFocus', block, true);
@@ -132,7 +132,7 @@
         scope.StreamInfosByUrl = [];
         scope.MainUrlByUrl = [];
         scope.EncodingCacheTimeout = 60000;
-        scope.DefaultProxyType = null;
+        scope.DefaultProxyType = 'TTV LOL';
         scope.DefaultForcedQuality = null;
         scope.DefaultProxyQuality = null;
     }
